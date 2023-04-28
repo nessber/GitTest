445fe6cf4488dc54e0d5bc00507da0b3c0fade7a
nessber <nestor.bernabeu@gmail.com>
2023-04-28 15:42:59 +0200

    Nuevo archivo index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// establecer la zona horaria a Madrid
+date_default_timezone_set('Europe/Madrid');
+
+// obtener la fecha actual
+$hoy = date('Y-m-d');
+
+// imprimir la fecha actual
+echo "La fecha de hoy es: " . $hoy;
+?>
